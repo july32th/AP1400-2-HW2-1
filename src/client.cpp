890abb49170f5af1b0e9c3f9bce07b2ec5ed6887
july32th <2766070760@qq.com>
2026-08-14 15:40:39 +0800

    init

--- src/client.cpp
+++ src/client.cpp
@@ -1,1 +1,18 @@
 #include "client.h"
+
+class Client
+{
+public:
+	Client(std::string id, const Server& server);
+	std::string get_id();
+	std::string get_publickey();
+	double get_wallet();
+	std::string sign(std::string txt);
+	bool transfer_money(std::string receiver, double value);
+	size_t generate_nonce();
+private:
+	Server const* const server;
+	const std::string id;
+	std::string public_key;
+	std::string private_key;
+};
